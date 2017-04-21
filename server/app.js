9f2d3a07d33894f1ba0e760d9b33b9d820cfedcd
Ke Zhang <plutino@gmail.com>
2017-04-20 20:14:22 -0700

    kz - remove view

--- server/app.js
+++ server/app.js
@@ -5,14 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./app/routes/index');
-var users = require('./app/routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
